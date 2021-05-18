c880a96d929ab90c73fd0c6b681bc081d2822fb7
Thanh Tu <thanhtu3274@gmail.com>
2021-05-18 20:37:55 +0700

    Create a.dart

---
+++ lib/a.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class A extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("xin chao cac ban");
+
+    // TODO: implement build
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
