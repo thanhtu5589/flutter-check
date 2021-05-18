4d1c1f397fd3165b57a24ba3438e6419201a6e57
Thanh Tu <thanhtu3274@gmail.com>
2021-05-18 20:40:20 +0700

    Update a.dart

--- lib/a.dart
+++ lib/a.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class A extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print("xin chao cac ban");
+    print("xin chao cac ban master print");
 
     // TODO: implement build
     return Scaffold(
