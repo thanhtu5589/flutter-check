d182037c56280304e7808342efdc60b45641fa5b
Thanh Tu <thanhtu3274@gmail.com>
2021-05-18 20:52:13 +0700

    M

--- lib/a.dart
+++ lib/a.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 class A extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print("xin chao cac ban beta print");
-    print("xin chao cac ban beta print again");
-    print("xin chao cac ban master print");
-    print("xin chao cac ban master print again");
 
     // TODO: implement build
     return Scaffold(
