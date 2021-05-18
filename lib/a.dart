6182034a0f2f7ab67c596e9caafa9cdf9a55a0cc
Thanh Tu <thanhtu3274@gmail.com>
2021-05-18 20:54:12 +0700

    B1

--- lib/a.dart
+++ lib/a.dart
@@ -4,6 +4,8 @@ class A extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    print("beta 1");
+
     // TODO: implement build
     return Scaffold(
       body: Container(),
