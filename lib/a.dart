b2bb777dedc7c1eabc9b8fb82c11d461f8ea7581
Thanh Tu <thanhtu3274@gmail.com>
2021-05-18 20:42:14 +0700

    M1

--- lib/a.dart
+++ lib/a.dart
@@ -4,7 +4,6 @@ class A extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print("xin chao cac ban master print");
-    print("xin chao cac ban beta print");
 
     // TODO: implement build
     return Scaffold(
